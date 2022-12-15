59dd3761c5f87121b0478920b063dc7bb14f60ff
Yan <Yan@DESKTOP-A3ENL6M>
2022-12-15 18:58:19 +0200

    rus coms

--- Heap sort/Heap sort.cpp
+++ Heap sort/Heap sort.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
 void heap(int arr[], int n, int i) {
-    // Find largest among root, left child and right child
+    // находим наибольшее число по всему древу
     int largest = i;
     int left = 2 * i + 1;
     int right = 2 * i + 2;
@@ -13,16 +13,16 @@ void heap(int arr[], int n, int i) {
     if (right < n && arr[right] > arr[largest])
         largest = right;
 
-    // Swap and continue heapifying if root is not largest
+    // Свапаем и продолжаем менять древо если корень не является наибольшем числом
     if (largest != i) {
         swap(arr[i], arr[largest]);
         heap(arr, n, largest);
     }
 }
 
-// main function to do heap sort
+// главная функция
 void heapSort(int arr[], int n) {
-    // Build max heap
+    // создаём пирамида или древо (max heap) то есть родитель всегда больше отпрыска
     for (int i = n / 2 - 1; i >= 0; i--)
         heap(arr, n, i);
 
@@ -30,19 +30,19 @@ void heapSort(int arr[], int n) {
     for (int i = n - 1; i >= 0; i--) {
         swap(arr[0], arr[i]);
 
-        // Heapify root element to get highest element at root again
+        // вызываем heap чтобы опять найти максимальный элемент в корне
         heap(arr, i, 0);
     }
 }
 
-// Print an array
+// Функция для печати 
 void printArray(int arr[], int n) {
     for (int i = 0; i < n; ++i)
         cout << arr[i] << " ";
     cout << "\n";
 }
 
-// Driver code
+
 int main() {
     int arr[] = { 1, 12, 9, 5, 6, 10 };
     int n = sizeof(arr) / sizeof(arr[0]);
